61d7de91018d3b61f346ba7401430073f7df24a0
Giosue1994 <giosuelentini1994@outlook.com>
2020-09-21 11:14:11 +0200

    modifica cornetto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="">Cornetti al pistacchio</a>
+                    <a href="">Cornetti al pistacchione</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
